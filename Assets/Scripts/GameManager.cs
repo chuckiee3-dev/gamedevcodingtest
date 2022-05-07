233087abd74ab5f378fbd4eaf5a1fa46decba59a
Chuckiee3 <cagri.benli@obvia.games>
2022-05-07 18:28:10 +0300

    Predicting using previous frame velocity of ball and players to determine which intersection occurs first.
    Added Line renderer for path visualization.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,13 +9,18 @@ public class GameManager : MonoBehaviour
     private GameObject ballPrefab;
     [SerializeField]
     private GameObject predictionVisualPrefab;
+    [SerializeField]
+    private LineRenderer visualizeRenderer;
 
     private GameObject ball;
     private GameObject[] players;
     private GameObject predictionVisual;
+    private Vector3[] playerPrevPositions;
+    private Vector2[] playerMovementVel;
     private Vector3 ballPrevPosition;
     private bool isFirstFrame;
     Vector3 ballMovementDir ;
+    
     void Start()
     {
         Data.Init();
@@ -26,6 +31,8 @@ public class GameManager : MonoBehaviour
         Data.SequenceMetaData = metaData;
         ball = Instantiate(ballPrefab);
         players = new GameObject[Data.TotalPlayers];
+        playerPrevPositions = new Vector3[Data.TotalPlayers];
+        playerMovementVel = new Vector2[Data.TotalPlayers];
         predictionVisual = Instantiate(predictionVisualPrefab);
         for (int i = 0; i < players.Length; i++)
         {
@@ -39,25 +46,71 @@ public class GameManager : MonoBehaviour
         Data.HighlightTime += Time.deltaTime * Data.StepsPerSecond;
         BallTransformSystem.Run(ball);
         PlayerTransformSystem.Run(players);
+        UpdatePrediction();
+    }
+
+    private void UpdatePrediction()
+    {
         if (isFirstFrame)
         {
             isFirstFrame = false;
             ballPrevPosition = ball.transform.position;
+            for (int i = 0; i < players.Length; i++)
+            {
+                var pos = players[i].transform.position;
+                playerPrevPositions[i] = players[i].transform.position;
+            }
             return;
         }
         ballMovementDir = ball.transform.position - ballPrevPosition;
         ballMovementDir.y = 0;
-        if(ballMovementDir.sqrMagnitude >= .001f){
-            PredictionSystem.Vector3DotRun(ball, players, predictionVisual, ballMovementDir.normalized);
+        UseVectorMath();
+    }
+
+    private void UseVectorMath()
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            Vector3 movement3D = players[i].transform.position - playerPrevPositions[i];
+            movement3D.y = 0;
+            playerMovementVel[i] = new Vector2(movement3D.x, movement3D.z) / Time.deltaTime;
         }
 
-        ballPrevPosition = ball.transform.position;
+        PredictionSystem.Run(ball, players, predictionVisual, ref playerMovementVel,
+            ballMovementDir / Time.deltaTime);
+        for (int i = 0; i < players.Length; i++)
+        {
+            playerPrevPositions[i] = players[i].transform.position;
+        }
+
+        if (ballMovementDir.sqrMagnitude > 0.0001f)
+        {
+            Vector3 start = ball.transform.position;
+            start.y = predictionVisual.transform.position.y;
+            visualizeRenderer.SetPositions(new []{start, predictionVisual.transform.position});
+            ballPrevPosition = ball.transform.position;
+        }
+        else
+        {
+            visualizeRenderer.SetPositions(new []{Vector3.zero, Vector3.zero});
+        }
     }
 
+
+
+
+#if  UNITY_EDITOR
     private void OnDrawGizmos()
     {
-        if(!ball) return;
+        /*if(!ball) return;
         Gizmos.color = Color.red;
         Gizmos.DrawLine(ball.transform.position, ball.transform.position + ballMovementDir.normalized * 5);
+        Gizmos.color = Color.blue;
+        for (int i = 0; i < players.Length; i++)
+        {
+            Gizmos.DrawLine(players[i].transform.position, 
+                players[i].transform.position + new Vector3(playerMovementVel[i].x,0 ,playerMovementVel[i].y) * 5);
+        }*/
     }
+#endif
 }
--- Assets/Scripts/PredictionSystem.cs
+++ Assets/Scripts/PredictionSystem.cs
@@ -3,56 +3,72 @@ using UnityEngine;
 
 public static class PredictionSystem
 {
-    private static float eligibleThreshold = .98f;
+    private static float eligibleThresholdBasicDot = .98f;
+    private static float eligibleThresholdLine = .95f;
+    private static float contactDistSq = .01f;
     private static readonly int ColorId = Shader.PropertyToID("_Color");
 
-    public static void Vector3DotRun(GameObject ball, GameObject[] players, GameObject predictionVisual,
+
+    public static void Run(GameObject ball, GameObject[] players, GameObject predictionVisual, 
+        ref Vector2[] playerMovementVel,
         Vector3 ballMovementDir)
     {
         var ballPosition = ball.transform.position;
         
-        Vector2 ballDir = new Vector2(ballMovementDir.x, ballMovementDir.z);
+        Vector2 ballVel = new Vector2(ballMovementDir.x, ballMovementDir.z).normalized;
         Vector2 ballPos2D = new Vector2(ballPosition.x, ballPosition.z);
-
-        float minDistSq = float.MaxValue;
-        int closestPlayerIndex = -1;
+       
+        int intersectionPlayerIndex = -1;
+        
+        Vector2 intersectionPos = Vector2.one * 100000f;
+        int calculatedIter = 0;
+        float distPlayerFinal = float.MaxValue;
         for (int i = 0; i < players.Length; i++)
         {
             Vector2 playerPos2D = new Vector2(players[i].transform.position.x, players[i].transform.position.z);
-            float dot = Vector2.Dot(ballDir.normalized, (playerPos2D - ballPos2D).normalized);
             
-            if(dot < eligibleThreshold) continue;
-            
-            float distSquared = (playerPos2D - ballPos2D).sqrMagnitude;
-            if (minDistSq > distSquared )
+
+            int maxIter = 300;
+            int iter=0;
+
+            Vector2 ballFuturePos = ballPos2D;
+            Vector2 playerFuturePos = playerPos2D;
+            float distPlayer = float.MaxValue;
+            while (iter < maxIter && distPlayer > contactDistSq)
             {
-                minDistSq = distSquared;
-                closestPlayerIndex = i;
+                if (playerMovementVel[i].sqrMagnitude > 0.0001f)
+                {
+                    playerFuturePos += playerMovementVel[i];
+                }
+
+                if (ballFuturePos.sqrMagnitude > 0.00001f)
+                {
+                    ballFuturePos += ballVel;
+                }
+                float newDist = (ballFuturePos - playerFuturePos).sqrMagnitude;
+                if (distPlayer >= newDist)
+                {
+                    distPlayer = newDist;
+                    if(distPlayer < distPlayerFinal)
+                    {
+                        distPlayerFinal = distPlayer;
+                        calculatedIter = iter + 1;
+                        intersectionPlayerIndex = i;
+                        intersectionPos = ballFuturePos;
+                    }
+                }
+                iter++;
             }
+            
         }
 
-        if (closestPlayerIndex != -1)
+        if (intersectionPlayerIndex != -1 && !float.IsNaN(intersectionPos.x) && !float.IsNaN(intersectionPos.y))
         {
-            VisualiseTargetedPlayer(players, closestPlayerIndex);
-            Vector2 predictedContactPos = (ball.transform.position + players[closestPlayerIndex].transform.position) / 2;
-            Vector3 targetPos = new Vector3(predictedContactPos.x, predictionVisual.transform.position.y,
-                predictedContactPos.y);
+            intersectionPos = ballPos2D + ballVel * calculatedIter;
+            Vector3 targetPos = new Vector3(intersectionPos.x, predictionVisual.transform.position.y,
+                intersectionPos.y);
             predictionVisual.transform.position = targetPos;
         }
     }
-
-    //Used for debugging only
-    private static void VisualiseTargetedPlayer(GameObject[] players, int closestPlayerIndex)
-    {
-        for (int i = 0; i < players.Length; i++)
-        {
-            if (i == closestPlayerIndex)
-            {
-                players[i].GetComponentInChildren<MeshRenderer>().material.SetColor(ColorId, Color.red);
-            }
-            else
-            {players[i].GetComponentInChildren<MeshRenderer>().material.SetColor(ColorId, Color.white);
-            }
-        }
-    }
+ 
 }
